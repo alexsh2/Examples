53c0522f5db3614d7418f6a2c40baeb320c7008a
Aleksandr_Shcherbakov <alexcht@mail.ru>
2023-01-15 17:46:06 +0300

    Add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 425;
+int c = 6;
+int d = 732;
+int e = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = c;
+
+Console.WriteLine($"max = {max}");
