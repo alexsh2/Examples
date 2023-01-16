885c5b4f481e839629f949efa0b5f9ca440e9f46
Aleksandr_Shcherbakov <alexcht@mail.ru>
2023-01-16 12:53:03 +0300

    Add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int[] array = { 1, 28, 3, 471, 5, 6, 3, 773, 80 };
+int index = 0;
+
+int n = array.Length;
+int find = 3;
+
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
